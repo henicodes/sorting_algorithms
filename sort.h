461b42f4232dfe657a12ff58bed9f84ffe8ff27f
henicodes <henicodes@gmail.com>
2022-08-16 18:01:18 +0000

    1. Insertion sort

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void print_list(const listint_t *list);
 /* Sorting Algorithms Functions */
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
